942dd44fbfe103504d037d9dcea07d6ce8a7433f
Will Sparger <will@wills-mbp.attlocal.net>
2016-11-10 16:10:19 -0600

    added matching logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,24 @@
 console.log("JS file is connected to HTML! Woo!")
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo) {
+alert("You found a match!")
+} else {
+alert ("Sorry, try again.")
+}
+
+if (cardOne === cardThree) {
+alert("You found a match!")
+} else {
+alert ("Sorry, try again.")
+}
+
+if (cardThree === cardFour) {
+alert("You found a match!")
+} else {
+alert ("Sorry, try again.")
+}
