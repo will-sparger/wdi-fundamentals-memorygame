c71868962b78d73e7dbac091bc3029d93f1318fc
Will Sparger <will@wills-mbp.attlocal.net>
2016-11-12 11:00:00 -0600

    added final logic for card game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,27 +1,6 @@
-console.log("JS file is connected to HTML! Woo!")
+var cards = ['queen', 'queen', 'king', 'king'];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-/*if (cardOne === cardTwo) {
-alert("You found a match!")
-} else {
-alert ("Sorry, try again.")
-}
-
-if (cardOne === cardThree) {
-alert("You found a match!")
-} else {
-alert ("Sorry, try again.")
-}
-
-if (cardThree === cardFour) {
-alert("You found a match!")
-} else {
-alert ("Sorry, try again.")
-}*/
+var cardsInPlay = [];
 
 var board = document.getElementById('game-board');
 
@@ -29,6 +8,35 @@ function createBoard() {
   for (var i=0; i<cards.length; i++) {
     var cardElement = document.createElement('div');
     cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+    board.appendChild(cardElement);
     board.appendChild(cardElement);
   }
+
 }
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>";
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>";
+	}
+  	if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
+
+createBoard();
+
