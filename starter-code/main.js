abfaf84b26522c0ea5c80a543679098536726ca6
Will Sparger <will@wills-mbp.attlocal.net>
2016-11-12 10:47:40 -0600

    added card creation javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,7 +5,7 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo) {
+/*if (cardOne === cardTwo) {
 alert("You found a match!")
 } else {
 alert ("Sorry, try again.")
@@ -21,4 +21,14 @@ if (cardThree === cardFour) {
 alert("You found a match!")
 } else {
 alert ("Sorry, try again.")
+}*/
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    board.appendChild(cardElement);
+  }
 }
